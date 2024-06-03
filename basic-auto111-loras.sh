ec400b7c955cad3e9be40ab2cbb9d7f8efa336e7
AlexisFabianCarranza <alexis.carranza.sistemas@gmail.com>
2024-06-03 17:44:41 -0300

    install loras and models

---
+++ basic-auto111-loras.sh
@@ -0,0 +1,10 @@
+wget -O /workspace/stable-diffusion-webui/models/Lora/Bubble Butt_alpha1.0_rank4_noxattn_last.safetensors https://civitai.com/api/download/models/533085?token=95fc7ee7e2a6ecc0c64f095ce9b18109
+wget -O /workspace/stable-diffusion-webui/models/Lora/breastsizeslideroffset.safetensors https://civitai.com/api/download/models/146600?token=95fc7ee7e2a6ecc0c64f095ce9b18109
+wget -O /workspace/stable-diffusion-webui/models/Lora/MissionaryVaginal-v2.safetensors https://civitai.com/api/download/models/183382?token=95fc7ee7e2a6ecc0c64f095ce9b18109
+wget -O /workspace/stable-diffusion-webui/models/Lora/PovDoggyAnal-v4.safetensors https://civitai.com/api/download/models/197444?token=95fc7ee7e2a6ecc0c64f095ce9b18109
+wget -O /workspace/stable-diffusion-webui/models/Lora/PovMissionaryAnal-v6.safetensors https://civitai.com/api/download/models/67485?token=95fc7ee7e2a6ecc0c64f095ce9b18109
+wget -O /workspace/stable-diffusion-webui/models/Lora/CloseUpVaginal-v1.safetensors https://civitai.com/api/download/models/65102?token=95fc7ee7e2a6ecc0c64f095ce9b18109
+wget -O /workspace/stable-diffusion-webui/models/Lora/Breast Slider - Pony_alpha1.0_rank4_noxattn_last.safetensors https://civitai.com/api/download/models/534952?token=95fc7ee7e2a6ecc0c64f095ce9b18109
+wget -O /workspace/stable-diffusion-webui/models/Lora/skin_tone_slider_v1.safetensors https://civitai.com/api/download/models/121575?token=95fc7ee7e2a6ecc0c64f095ce9b18109
+wget -O /workspace/stable-diffusion-webui/models/Lora/Cleavage Slider_alpha1.0_rank4_noxattn_last.safetensors.safetensors https://civitai.com/api/download/models/541905?token=95fc7ee7e2a6ecc0c64f095ce9b18109
+wget -O /workspace/stable-diffusion-webui/models/Lora/Body Type_alpha1.0_rank4_noxattn_last.safetensors https://civitai.com/api/download/models/520909?token=95fc7ee7e2a6ecc0c64f095ce9b18109
--- basic-auto111-models.sh
+++ basic-auto111-models.sh
@@ -3,3 +3,5 @@ wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/omnigenxlNSFWS
 wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/wildcardxREALNSFWSFW_nsfwSFW.safetensors https://civitai.com/api/download/models/342799?token=95fc7ee7e2a6ecc0c64f095ce9b18109
 wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/uberRealisticPornMerge_urpmv13.safetensors https://civitai.com/api/download/models/15640
 wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/realisticVisionV60B1_v51HyperVAE.safetensors https://civitai.com/api/download/models/501240
+wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/chilloutmix_NiPrunedFp32Fix.safetensors https://civitai.com/api/download/models/11745?token=95fc7ee7e2a6ecc0c64f095ce9b18109
+wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/ponyRealism_v21VAE.safetensors https://civitai.com/api/download/models/534642?token=95fc7ee7e2a6ecc0c64f095ce9b18109
